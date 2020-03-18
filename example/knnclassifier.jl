366e75659334fc4d09524d71c2f042d3c8f3a00a
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2020-03-18 14:34:09 -0300

    Add knnclassifier example

---
+++ example/knnclassifier.jl
@@ -0,0 +1,25 @@
+using MeLOne
+using Plots
+gr(size=(800,600))
+
+function knnclassifier()
+  n = 400
+  X = 2 * rand(n, 2)
+  y = [X[i,1].^2 + X[i,2].^2 < 1.5 + randn() * 0.6 ? 0 : 1 for i = 1:n]
+
+  model = KNNClassifier(n_neighbors=13)
+  fit!(model, X, y)
+
+  xg = range(minimum(X[:,1]), maximum(X[:,1]), length=50)
+  yg = range(minimum(X[:,2]), maximum(X[:,2]), length=50)
+  pred(x,y) = predict(model, [x y])[1]
+
+  heatmap(xg, yg, pred, c=cgrad([:pink, :lightblue]))
+  scatter!(X[:,1], X[:,2], c=y, leg=false)
+  y_pred = predict(model, X)
+  title!("acc = $(accuracy_score(y, y_pred))")
+
+  png("knnclassifier")
+end
+
+knnclassifier()
