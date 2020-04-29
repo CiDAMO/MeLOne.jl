461f0e9e403a41d1341c9bccf17135349d4257a0
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2020-04-28 21:20:12 -0300

    Remove signatures from API

--- src/api.jl
+++ src/api.jl
@@ -8,18 +8,14 @@ abstract type MeLOneModel end
 
 Fit the `model` with matrix `X` and target `y`.
 """
-function fit!(model :: MeLOneModel, X :: Matrix, y :: Vector)
-  error("This model does not define fit!(model, X, y)")
-end
+function fit! end
 
 """
     y_pred = predict(model, X)
 
 Predict the target of matrix `X` with `model`. `y_pred` is a Vector.
 """
-function predict(model :: MeLOneModel, X :: Matrix)
-  error("This model does not define predict(model, X)")
-end
+function predict end
 
 """
     prob_mat = predict_proba(model, X)
@@ -28,6 +24,4 @@ Predict the probability of each row of matrix `X` being of each class
 in `model._classes`. `prob_mat` is a matrix with dimensions `size(X, 1)`
 by `length(model._classes)`. Each row of `prob_mat` that sum 1.
 """
-function predict_proba(model :: MeLOneModel, X :: Matrix)
-  error("This model does not define predict_proba(model, X)")
-end
+function predict_proba end
